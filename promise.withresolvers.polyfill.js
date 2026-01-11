2c895721009b4396239baf63823798b4986328c8
IanoNJuguna <inc3099@gmail.com>
2026-01-11 23:58:29 +0300

    add a polyfill for Promise.withResolvers for compatibility with all browsers and devices

---
+++ promise.withresolvers.polyfill.js
@@ -0,0 +1,11 @@
+// Polyfill for Promise.withResolvers for compatibility with browsers/environments that do not support it natively.
+if (typeof Promise.withResolvers !== 'function') {
+  Promise.withResolvers = function withResolvers() {
+    let resolve, reject;
+    const promise = new Promise((res, rej) => {
+      resolve = res;
+      reject = rej;
+    });
+    return { promise, resolve, reject };
+  };
+}
